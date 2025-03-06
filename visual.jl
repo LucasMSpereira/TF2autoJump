c862020ed2b6d6629c6a75de9918e2b89e3b42ab
Lucas Pereira <lucas.lmspereira@gmail.com>
2025-03-06 01:31:46 -0300

    progress on interactivity

--- visual.jl
+++ visual.jl
@@ -1,21 +1,29 @@
-begin
-  # hex = hexas[293]
-  fig = Figure()
-  ax = Axis3(fig[1, 1])
-  # viz(hex, showsegments = true, showpoints = true)
-  for hex in hexas
-    coords = [[vert.coords.x.val, vert.coords.y.val, vert.coords.z.val] for vert in hex.vertices]
-    coordMat = hcat(coords...)
-    ranges = extrema(coordMat; dims = 2) .|> collect .|> diff .|> only .|> abs |> vec
-    origin = minimum(coordMat; dims = 2) |> vec
-    rec = Rect3d(origin, ranges)
-    m = mesh!(ax, rec)
-    wireframe!(ax, m[1], color = :black)
+with_theme(theme_dark()) do
+  fig = Figure(; size = (1700, 700))
+  lscene = LScene(fig[1, 1]; show_axis=false)
+  m = 0
+  hObs = Observable(Rect3d.(hexas))
+  for (id, hex) in enumerate(hexas)
+    # id in [346, 352, 64, 66, 63, 350, 69, 351, 349] && continue
+    m = mesh!.(lscene, hObs[]; alpha = 0.1)
+  end
+  obs = Observable{Any}(0.0)
+  on(events(lscene).mousebutton, priority = 2) do event
+    if event.button == Mouse.left && event.action == Mouse.press
+      if Keyboard.left_shift in events(fig).keyboardstate # print id
+        plt = pick(m)[1]
+        findfirst(==(plt.deregister_callbacks[2].observable[]), hexas) |> println
+      elseif Keyboard.left_control in events(fig).keyboardstate # remove hexahedron
+        plt = pick(m)[1]
+        id = findfirst(==(plt.deregister_callbacks[2].observable[]), hexas) |> println
+        deleteat!(hObs[], id)
+        notify(hObs)
+        # return Consume(true)
+      end
+    end
   end
   fig
 end
-k = 135
-@time display(Meshes.viz(hexas[1:end - k]; color = 1:length(hexas) - k))
 
 fig = Figure(resolution=(900,900));
 extraFrames = 7 # odd integer. how many frames per topology
@@ -30,11 +38,3 @@ fig, ax, hm = heatmap(1:problem_size[1], 1:problem_size[2], topos[:,:,1])
 record(fig, "TOBS.mp4", 1:size(topos,3)) do i
   hm[3] = topos[:,:,i] # update data
 end
-
-# k = 135
-# fig = Figure(resolution=(900,900));
-# frames = [MeshViz.viz(hexas[1:p]) for p in 1:length(hexas)-k];
-# record(fig, "beef.mp4", 1:length(hexas)-k) do i
-#   x = frames[i]; # update data
-# end
-# b = MeshViz.viz(hexas[1]);
